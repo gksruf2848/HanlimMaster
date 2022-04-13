3ad449b9191b2d54247e7f2023f82bd4e54a567a
gksruf2848 <4455qqq@naver.com>
2022-04-13 10:59:46 +0900

    Create commit

---
+++ BasicSkills/Generic/src/main/java/org/tain/test01/common/Car.java
@@ -0,0 +1,14 @@
+package org.tain.test01.common;
+
+public class Car {
+	
+	private String name;
+	
+	public Car() { };
+	
+	public Car(String name) { this.name = name; }
+
+	public String getName() { return this.name; }
+	
+	public String toString() { return String.format("[Car : %s]", this.name); }
+}
---
+++ BasicSkills/Generic/src/main/java/org/tain/test01/common/Tv.java
@@ -0,0 +1,15 @@
+package org.tain.test01.common;
+
+public class Tv {
+	
+	private String name;
+	
+	public Tv() { };
+	
+	public Tv(String name) { this.name = name; }
+	
+	public String getName() { return this.name; }
+	
+	public String toString() { return String.format("[Tv : %s]", this.name); }
+
+}
